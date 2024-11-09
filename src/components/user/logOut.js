45dd58c61ea278fceed1bac588c116b85824fc37
ShrutiGupta <ShrutiGupta1604@gmail.com>
2024-11-09 16:46:48 +0530

    first commit

---
+++ src/components/user/logOut.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function LogOut() {
+    return (
+        <div>
+            Login Page
+            login
+            email id
+            password
+        </div>
+    )
+}
+
+export default LogOut;
